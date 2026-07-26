a9b39eb571187bf3a71c4d3c14a429d6d3403418
Ar-012 <archt_12@outlook.com>
2026-07-26 18:28:05 +0800

    Add frosted glass treatment to today card

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lizi-growth-v41";
+const CACHE_NAME = "lizi-growth-v42";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png", "/og.png"];
 
 self.addEventListener("install", (event) => {
