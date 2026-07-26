322fdb226090dbd0511c7b1a64b4b3a21937e73d
Ar-012 <archt_12@outlook.com>
2026-07-26 22:02:08 +0800

    Neutralize dark mode palette

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lizi-growth-v79";
+const CACHE_NAME = "lizi-growth-v80";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png", "/og.png"];
 
 self.addEventListener("install", (event) => {
