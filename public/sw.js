d536766dcdd9fe5dc29ee532396a5bd1f07dd03b
Ar-012 <archt_12@outlook.com>
2026-07-27 12:41:25 +0800

    Increase small interface text

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lizi-growth-v102";
+const CACHE_NAME = "lizi-growth-v103";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png", "/og.png"];
 
 self.addEventListener("install", (event) => {
